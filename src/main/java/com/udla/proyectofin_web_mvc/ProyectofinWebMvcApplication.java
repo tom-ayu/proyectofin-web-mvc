6fdb295f66b1d91177c6e510d0654bacc4227c6c
tom-ayu <tomas.ontaneda@udla.edu.ec>
2025-10-02 20:21:51 -0500

    Primer commit: Creación Proyecto Java 21 + Spring Boot 3.5.6 (y dependencias)

---
+++ src/main/java/com/udla/proyectofin_web_mvc/ProyectofinWebMvcApplication.java
@@ -0,0 +1,13 @@
+package com.udla.proyectofin_web_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectofinWebMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectofinWebMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udla/proyectofin_web_mvc/ProyectofinWebMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udla.proyectofin_web_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectofinWebMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
